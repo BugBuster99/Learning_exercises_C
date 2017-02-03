029a078eb1aa902081d9e8e88fd326587422211c
BugBuster99 <arvindj19962014@gmail.com>
2017-02-03 18:16:40 +0530

    Update SQplay.c

--- SQplay.c
+++ SQplay.c
@@ -1,4 +1,22 @@
 #include<stdio.h>
+int checkwin(char *a[4][4])
+{
+    int i,j,temp=1;
+    for(i=0;i<4;i++)
+    {
+        for(j=0;j<4;j++)
+        {
+            if(i==3 && j==3 && a[i][j]==' ')
+            {
+                return 1;
+            }
+            if((a[i][j])!=temp)
+            {
+                return 0;
+            }
+        }
+    }
+}
 void main()
 {
 	char a[4][4];
@@ -17,7 +35,10 @@ void main()
 	i=3;
 	j=3;
 	a[i][j]=' ';
-    for(k=0;k<4;k++)
+    flag=0;
+	while (ch!='Q'&&flag==0)
+	{
+	    for(k=0;k<4;k++)
 	{
 	    printf("-------------------\n|");
 		for(l=0;l<4;l++)
@@ -34,8 +55,6 @@ void main()
 		printf("\n");
 	}	
 	printf("-------------------");
-	while (ch!='Q'&&flag==0)
-	{
 		printf("\n\nU-up D-down L-left R-right\nEnter Your choice:");
 		scanf("%c",&ch);
 		printf("\n");
@@ -56,8 +75,8 @@ void main()
 			}
 			break;
 			case 'D':
-			a[i][j]=a[i+1][j];
-			i=(i+1);
+			a[i][j]=a[(i+1)%4][j];
+			i=(i+1)%4;
 			a[i][j]=' ';
 			break;
 			case 'L':
@@ -74,13 +93,14 @@ void main()
 			}
 			break;
 			case 'R':
-			a[i][j]=a[i][(j+1)];
+			a[i][j]=a[i][(j+1)%4];
 			j=(j+1)%4;
 			a[i][j]=' ';
 			break;
 			case 'Q':
 			break;			
 		}
+		flag=checkwin(&a);
 		if(flag==1)
         {
         for(k=0;k<4;k++)
@@ -99,7 +119,7 @@ void main()
 		    }
 		    printf("\n");
 	    }
-	printf("-------------------");	    
+	printf("-------------------\n\nCongrats!!!! You Won!!");	    
 	}
 	}
 } 
