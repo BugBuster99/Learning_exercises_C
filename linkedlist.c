3bc0d601a37cb88e945553702738bbadfaea4480
BugBuster99 <arvindj19962014@gmail.com>
2017-03-24 11:11:07 +0530

    Update linkedlist.c

--- linkedlist.c
+++ linkedlist.c
@@ -6,7 +6,7 @@ struct node{
     struct node* next;
 };
 
-void print_list(struct node* head){
+void print_list(struct node* head){				//Function to print the list
     struct node* traverser=head;
     while((traverser)!=NULL){
         printf("%d\n",traverser->data);
@@ -14,7 +14,7 @@ void print_list(struct node* head){
     }
 }
 
-void add(struct node* head,struct node* new_node){
+void add(struct node* head,struct node* new_node){		//Function to add new values
     struct node* traverser;
     traverser=head;
     while((traverser->next)!=NULL){
@@ -26,12 +26,12 @@ void add(struct node* head,struct node* new_node){
 struct node* create_list(int n){
     int i;
     struct node* head = NULL;
-    struct node* first_node;
+    struct node* first_node;					//First node is created
     first_node=(struct node*)malloc(sizeof(struct node));
 	scanf("%d",&(first_node->data));
 	first_node->next=NULL;
 	head=first_node;
-	for(i=1;i<n;i++){
+	for(i=1;i<n;i++){					//Successive nodes are created using add function in the loop
 	struct node* new_node=(struct node*)malloc(sizeof(struct node));
 	scanf("%d",&(new_node->data));
 	new_node->next=NULL;
@@ -45,30 +45,29 @@ void check(struct node* head,int x,int n){
     struct node* traverser;
     traverser=head;
     int count=0,flag=1;
-    while(count<n){
-        
+    while(count<n){					//Function to traverse and find the required element
         if((traverser->data)==x){
-            printf("The index is %d\n%d\n",traverser->data,count);
+            printf("\nThe index is %d\n%d\n",traverser->data,count);
             traverser=traverser->next;
             
-            flag=0;
+            flag=0;					//If the element is found loop is terminated
             break;
         }
         count++;
     }
     if(flag!=0){
-        printf("Element Not Found!!!");
+        printf("Element Not Found!!!");			//If the element doesnot exist
     }
 }
 
 int main() {
 	int n,x,val;
 	struct node* head;
-	head=(struct node*)malloc(sizeof(struct node));
-	printf("Enter the number nodes\n");
+	head=(struct node*)malloc(sizeof(struct node));		
+	printf("Enter the number nodes\n");			//Number of nodes is got from user
 	scanf("%d",&n);
 	printf("Enter the data\n");
-	head=create_list(n);
+	head=create_list(n);					//Function to create list is called
 	printf("Enter the value to check");
 	scanf("%d",&val);
 	check(head,x,val);
